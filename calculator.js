61955157c9c329fade5dd1f2d6c65fbabe856ec9
sukhanna <sumeet.friends@gmail.com>
2020-02-25 23:36:28 +0530

    evaluate expression. brackets are not handled. operators at the begining of an expression are not handled.

--- calculator.js
+++ calculator.js
@@ -5,13 +5,15 @@
 /* eslint-disable no-use-before-define */
 
 function parseExpr(expr) {
+  // minus in the begining of expression fails i.e. -2 + 3 gives 5 instead of 1
   if (expr === "") {
     console.error("Emtpy expression. Please provide valid expression");
-    return;
+    return expr;
   }
-  const postfixExpr = convertToRpn(expr);
-  console.log(`RPL expression : ${postfixExpr}`);
-  return evaluateExpr(postfixExpr);
+  const postfixExprArray = convertToRpn(expr);
+  const result = evaluateExpr(postfixExprArray);
+  console.log(`RPN expression : ${postfixExprArray} evaluates to ${result}`);
+  return result;
 }
 
 function convertToRpn(expr) {
@@ -21,12 +23,17 @@ function convertToRpn(expr) {
 
   const opStack = [];
   const outQueue = [];
+  let tokenizer = [];
 
   for (const char of expr) {
     if (isOperand(char)) {
-      outQueue.push(char);
+      tokenizer.push(char);
     }
     else if (isOperator(char)) {
+      if (tokenizer.length > 0) {
+        outQueue.push(tokenizer.reduce((accum, curValue) => accum + curValue));
+        tokenizer = [];
+      }
       while (opStack.length !== 0
         && opPrecedence[opStack[opStack.length - 1]] >= opPrecedence[char]) {
         outQueue.push(opStack.pop());
@@ -45,20 +52,53 @@ function convertToRpn(expr) {
     */
   }
 
+  if (tokenizer.length > 0) {
+    outQueue.push(tokenizer.reduce((accum, curValue) => accum + curValue));
+    tokenizer = [];
+  }
+
   while (opStack.length !== 0) {
     outQueue.push(opStack.pop());
   }
   return outQueue;
 }
 
-function evaluateExpr(expr) {
-  return expr;
+function evaluateExpr(postfixExprArray) {
+  const operandStack = [];
+  postfixExprArray.forEach((element) => {
+    if (isOperand(element)) {
+      operandStack.push(element);
+    }
+    else if (isOperator(element)) {
+      if (operandStack.length <= 1) {
+        console.error(`Operation : ${element} requires two operand. Second Operand is missing`);
+        return operandStack.toString();
+      }
+      switch (element) {
+        case "*":
+          operandStack.push(Number(operandStack.pop()) * Number(operandStack.pop()));
+          break;
+        case "/":
+          operandStack.push(Number(operandStack.pop()) / Number(operandStack.pop()));
+          break;
+        case "+":
+          operandStack.push(Number(operandStack.pop()) + Number(operandStack.pop()));
+          break;
+        case "-":
+          operandStack.push(Number(operandStack.pop()) - Number(operandStack.pop()));
+          break;
+        default:
+          console.error(`operation not supported: ${element}`);
+      }
+    }
+  });
+  return operandStack.toString();
 }
 
 function isOperand(char) {
-  return char.match(/[0-9]/) !== null;
+  return /[0-9]+/.test(char);
 }
 
 function isOperator(char) {
-  return char.match(/\*|\/|\+|-/) !== null;
+  return /\*|\/|\+|-/.test(char);
 }
