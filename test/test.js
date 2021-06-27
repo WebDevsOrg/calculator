00a91689e6cf7ce839ff6049a68f36a628c0cfe0
Sumeet <sumeeet.khanna@gmail.com>
2021-06-27 20:27:32 +0530

    test case for decimal operation and divide

--- test/test.js
+++ test/test.js
@@ -16,6 +16,27 @@ describe("Calculator", () => {
     it("should return 2000000 when the expression is 1000000 + 1000000", () => {
       assert.strictEqual(claculator.parseExpr("1000000+1000000"), 2000000);
     });
+
+    it("should return 3 when the expression is +3", () => {
+      assert.strictEqual(claculator.parseExpr("+3"), 3);
+    });
+
+    it("should return 3 when the expression is +*3", () => {
+      assert.strictEqual(claculator.parseExpr("+*3"), 3);
+    });
+
+    it("should return 3 when the expression is 3+", () => {
+      assert.strictEqual(claculator.parseExpr("3+"), 3);
+    });
+
+
+    it("should return 23 + 0.23 when the expression is 23 + 0.23", () => {
+      assert.strictEqual(claculator.parseExpr("23 + 0.23").toFixed(2), "23.23");
+    });
+
+    it("should return 2^3 when the expression is 2^3", () => {
+      assert.strictEqual(claculator.parseExpr("2^3"), "2^3");
+    });
   });
 
   describe("TestSubtractExpression()", () => {
@@ -29,6 +50,33 @@ describe("Calculator", () => {
 
     it("should return 1 when the expression is 1 - 0", () => {
       assert.strictEqual(claculator.parseExpr("1-0"), 1);
+    });
+
+    it("should return 0.3 when the expression is 0.5 - 0.2", () => {
+      assert.strictEqual(claculator.parseExpr("0.5 - 0.2"), 0.3);
+    });
+
+    it("should return -1 when the expression is -3 + 2", () => {
+      assert.strictEqual(claculator.parseExpr("-3 + 2"), -1);
+    });
+
+    it("should return -3 when the expression is -3", () => {
+      assert.strictEqual(claculator.parseExpr("-3"), -3);
+    });
+  });
+
+  describe("TestDivideExpression()", () => {
+    it("should return 0 when the expression is 0 / 5", () => {
+      assert.strictEqual(claculator.parseExpr("0/5"), 0);
+    });
+
+    it("result is undefined and should return 0 when the expression is 0 - 0", () => {
+      assert.strictEqual(claculator.parseExpr("0/0"), 0);
+    });
+
+    it("should return 0.33333 when expression is 1/3", () => {
+      // toFixed() returns a string
+      assert.strictEqual(claculator.parseExpr("1/3").toFixed(5), "0.33333");
     });
   });
 });
